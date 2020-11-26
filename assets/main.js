a3f9435f7249159d5df908cbb15c9ab67117970d
arifmr <arifmr@pop-os.localdomain>
2020-11-27 05:02:14 +0700

    adding register features and oauth google login 1. adding register page in index.html 2. adding google oauth login button and script in index.html 3. adding register function and showRegisterPage function in assets/helper.js 4. adding function onSignIn function in assets/helper.js 5. adding register submit function event listener in assets/main.js

--- assets/main.js
+++ assets/main.js
@@ -1,17 +1,36 @@
 $(document).ready(function() {
+    if(localStorage.getItem('access_token')) {
+        showMainPage()
+    } else {
+        showLoginPage()
+    }
+
+    $('#register-form').on("submit", function(e) {
+        e.preventDefault()
+        register()
+    })
 
     $('#login-form').on("submit", function(e) {
         e.preventDefault()
         login()
     })
-    
+
     $('#toLoginForm').on("click", function(e) {
         e.preventDefault()
         showLoginPage()
     })
-
+    
+    $('#toRegisterForm').on("click", function(e) {
+        e.preventDefault()
+        showRegisterPage()
+    })
+    
     $('#btn-logout').on("click", function(e) {
         logout()
     })
 
+    $('#btn-adopt').on("click", function(e) {
+        e.preventDefault()
+        showAdoptCatsPage()
+    })
 })
